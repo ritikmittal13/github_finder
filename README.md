8b92988b0dba65a8fa0d1c4470090dcbeba68e0a
Ritik Mittal <ritikmittal13@gmail.com>
2020-05-23 01:02:58 +0530

    Alert added

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-Simple Github Profile Locator using name or username.
-Just type your query in the search field and find all the related profiles!
-Click on more to redirect to their GitHub profiles.
+# Github Finder
+
+> React app to search Github profiles. This app uses the Context API along with the useContext and useReducer hooks for state management
+
+## Usage
+
+### `npm install`
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000)
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -5,16 +5,24 @@ export class Search extends Component {
   state = {
     text: "",
   };
+
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert(" Please enter your query", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
