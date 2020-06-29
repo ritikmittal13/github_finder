f94e62b588cb9892960ac9fdf5a8f930c7572851
Ritik Mittal <ritikmittal13@gmail.com>
2020-06-30 02:36:02 +0530

    Added Context

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='leads'>
+        The page you are trying to look for does not exist...
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,14 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos";
-import UserItem from "./UserItem";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ getUser, getUserRepos, loading, user, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, user, getUser, getUserRepos, repos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -95,14 +98,6 @@ const User = ({ getUser, getUserRepos, loading, user, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-UserItem.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired,
 };
 
 export default User;
